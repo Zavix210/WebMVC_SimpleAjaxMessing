96feec80bd34a4768f2aff86232609fc6d32d36b
jack walker <jack.w936@googlemail.com>
2018-09-25 23:02:27 +0100

    Add project files.

---
+++ WebApplication4/Controllers/NoteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Note/Details/5
+        public ActionResult Details(int id)
+        {
+            NoteViewModel a = new NoteViewModel()
+            {
+                first = new decimal(23.2),
+                second = DateTime.MinValue, 
+                third = 22,
+                fourth = "",
+                fifth = "", 
+                sixth = "", 
+                seventh = "", 
+                eigth = "", 
+                nineth = DateTime.MaxValue
+            };
+            IEnumerable<NoteViewModel> n = new List<NoteViewModel>()
+            {
+                a
+            };
+            return PartialView(n);
+        }
+
+        // GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Note/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Note/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+    }
+}
---
+++ WebApplication4/Models/NoteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class NoteViewModel
+    {
+        public Decimal first { get; set; }
+        public DateTime second { get; set; }
+        public int third { get; set; }
+        public string fourth { get; set; }
+        public string fifth { get; set; }
+        public string sixth { get; set; }
+        public string seventh { get; set; }
+        public string eigth { get; set; }
+        public DateTime nineth { get; set; }
+    }
+}
